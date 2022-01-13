94e687c311d065138e85eed3db5ff5175a3f1dae
mathaus <mathaus.aquicob@gmail.com>
2022-01-13 19:14:30 -0300

    testing again

--- src/Traits/Crudable.php
+++ src/Traits/Crudable.php
@@ -46,8 +46,6 @@ trait Crudable {
         $array = [];
         if (is_object($returnable) || is_array($returnable)) {
             foreach ($returnable as $key => $returnable_value) {
-
-                dd($returnable[$key], $key, $returnable_value);
                 if (is_string($returnable_value)) {
                     $array[iconv("ISO-8859-1", "UTF-8", $key)] = iconv("ISO-8859-1", "UTF-8", $returnable_value);
                 } else if (is_object($returnable_value) || is_array($returnable_value)) {
@@ -56,6 +54,7 @@ trait Crudable {
             }
         }
 
+        dd($array);
         return $array;
     }
 }
