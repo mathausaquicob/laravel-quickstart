8f66256b16fddee98fa8223c33b97aa59faab334
mathaus <mathaus.aquicob@gmail.com>
2022-01-13 22:27:27 -0300

    another debug

--- src/Traits/Crudable.php
+++ src/Traits/Crudable.php
@@ -51,7 +51,7 @@ trait Crudable {
             }
 
             foreach ($returnable->getRelations() as $rel_key => $relationship) {
-                dd($returnable->{$relationship});
+                dd($returnable->{$relationship}, $relationship);
                 $array[iconv("ISO-8859-1", "UTF-8", $rel_key)] = $this->iso8859toutf8($returnable->{$relationship});
             }
 
